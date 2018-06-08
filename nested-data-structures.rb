4a9788fff18575861d4007d153b5fcb3fcbd8d38
Samantha Cassity <cassityclaire@gmail.com>
2018-06-08 18:55:51 +0000

    automatically backed up by learn

--- nested-data-structures.rb
+++ nested-data-structures.rb
@@ -1,7 +1,7 @@
 #create a hash 
 group_sam { :sam => ["ocean
 _eyes", "lithium", "24/7"],
-:natalia => ["bloom", "stay", "lovely"]
+:natalia => ["bloom", "stay", "lovely"],
 :ashlee => ["white_rabbit", "for_forever", "full_disclosure"]
 }
 puts group_sam[:sam][1]
