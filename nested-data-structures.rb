8a4f12e13dd6aee12d6ca88797dd98266e41d93a
Samantha Cassity <cassityclaire@gmail.com>
2018-06-08 18:55:33 +0000

    automatically backed up by learn

--- nested-data-structures.rb
+++ nested-data-structures.rb
@@ -2,5 +2,6 @@
 group_sam { :sam => ["ocean
 _eyes", "lithium", "24/7"],
 :natalia => ["bloom", "stay", "lovely"]
-:ashlee => ["white_rabbit", "", ""]
+:ashlee => ["white_rabbit", "for_forever", "full_disclosure"]
 }
+puts group_sam[:sam][1]
